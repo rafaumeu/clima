a31392f0420194fbd5abe56da9e212e285527b92
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-01-03 13:20:49 -0300

    feat: integração de api
    integração de api para ficar disponivel para consultas e preenchimento de forma dinamica da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,28 @@ import { Titulo } from './AppStyles.js'
 import Busca from './components/Busca'
 import ClimaAtual from './components/ClimaAtual'
 import Previsao from './components/Previsao.jsx'
+import config from '../config.js'
 
 function App() {
   const [cidade, setCidade] = useState('')
   const [clima, setClima] = useState(null)
   const [previsao, setPrevisao] = useState([])
+  const apiKey = config.apiKey
+  const buscarClima = async () => {
+    try {
+      const respostaClima = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}`
+      )
+      setClima(respostaClima.data)
+    } catch (error) {
+      console.log('Erro ao buscar clima: ' + error)
+    }
+  }
+
   return (
     <div>
       <Titulo>Condições Climáticas</Titulo>
-      <Busca />
+      <Busca cidade={cidade} setCidade={setCidade} buscarClima={buscarClima} />
       <ClimaAtual />
       <Previsao />
     </div>
